5073b14915fe5970106010d0324a64fad2b6c999
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-11-11 15:31:40 -0300

    Send props from father component to child

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Header() {
+// maneras de recibir this.props.
+// function Header(props)
+// function Header({prop1, prop2, propn})
+export default function Header(props) {
+    const {name:nameProp, email} = props;
+
+    // const {name:nameProp, email} = props;
+    console.log({email})
+
+    const [name, setName] = useState(nameProp);
+
+    const changeName=()=>{
+        setName('Nicolas Henriquez');
+    }
+
   return (
     <div>
-        <h2>Ricardo Urvina</h2>
+        <h2>{name}</h2>
+        <br></br>
+        <span>Correo: {email}</span>
+        <br></br>
+        <button onClick={()=>changeName()}>Change Name</button>
     </div>
   )
 }
